f69122bfffdc4f00f38a88fa6985371f3fb482b1
MahAzevedo <madavetto@gmail.com>
2025-06-29 22:02:00 -0300

    new file:   src/Components/Header/Header.jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from './assets/Logo.jpg';
+
+export default function Header() {
+    return (
+        <img src={Logo} alt='Logo do Ecommerce' />
+        <nav>
+            <li></li>
+        </nav>
+    )
+}
