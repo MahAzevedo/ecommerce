397b0723e6e1c1f262dde1461b75a36db45a6d92
MahAzevedo <madavetto@gmail.com>
2025-06-30 22:30:25 -0300

    modified:   src/Components/Header/Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,9 +2,20 @@ import Logo from './assets/Logo.jpg';
 
 export default function Header() {
     return (
-        <img src={Logo} alt='Logo do Ecommerce' />
-        <nav>
-            <li></li>
-        </nav>
+        <div>
+            <div>Jewelry are forever!</div>
+                <img src={Logo} alt='Logo do Ecommerce' />
+                <nav>
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>Comprar</a>
+                    </li>
+                    <li>
+                        <a href='#'>Login</a>
+                    </li>
+                </nav>
+        </div>
     )
 }
